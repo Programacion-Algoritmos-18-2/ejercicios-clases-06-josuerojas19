127f135ffd6a4da8bf908ecaac6d394e97075812
josuerojas19 <43886172+josuerojas19@users.noreply.github.com>
2018-11-16 11:22:17 -0500

    Update modelo2.py

--- modelo2.py
+++ modelo2.py
@@ -1,35 +1,43 @@
 import abc
+#Clase Padre y Abstracta
 class Persona_Equipo(metaclass=abc.ABCMeta):
-
+   
     def __init__(self, nom):
         self.nombre = nom
-
+    #Metodos set y get
     def setNombre(self, n):
         self.nombre = n
 
     def getNombre(self):
         return self.nombre
-
+    #Metodo Abstracto
     @abc.abstractmethod
     def entrenamiento(self):
         pass
 
+#Clase Hija que hereda de Persona_Equipo
 class Entrenador(Persona_Equipo):
     def __init__(self, nom):
         super(Entrenador, self).__init__(nom)
         self.codigo_entrenador = ""
+    
+    #Metodos get y set    
     def setCodigo(self ,c):
         self.codigo_entrenador = c
+        
     def getCodigo(self):
         return self.codigo_entrenador
+    
     def entrenamiento(self):
         print("Yo", self.getNombre(), ",soy el entrenador, Dirijo el etrenamiento")
-
+        
+#Clase Hija que hereda de Persona_Equipo
 class Futbolista(Persona_Equipo):
     def __init__(self, nom):
         super(Futbolista, self).__init__(nom)
         self.posicion_campo = ""
-
+    
+    #Metodos get y set
     def setPosicion(self, p):
         self.posicion_campo = p
 
@@ -38,24 +46,30 @@ class Futbolista(Persona_Equipo):
 
     def entrenamiento(self):
         print("Yo", self.getNombre(), ",futbolista, hago Practica en el entrenamiento")
-
+        
+#Clase Hija que hereda de Persona_Equipo
 class MedicoEquipo(Persona_Equipo):
     def __init__(self, nom):
         super(MedicoEquipo, self).__init__(nom)
         self.titulo = ""
+   
+    #Metodos get y set
     def setTitulo(self, t):
         self.titulo = t
 
     def getTitulo(self):
         return self.titulo
+    
     def entrenamiento(self):
         print("Yo", self.getNombre(), ",medico,atiendo los jugadores en el etrenamiento")
 
+#Clase Hija que hereda de Persona_Equipo        
 class PresidenteEquipo(Persona_Equipo):
     def __init__(self, nom):
         super(PresidenteEquipo, self).__init__(nom)
         self.numpropiedades = 0
-
+    
+    #Metodos get y set
     def setNumeroPropiedades(self, num):
         self.numpropiedades = num
 
@@ -65,7 +79,7 @@ class PresidenteEquipo(Persona_Equipo):
     def entrenamiento(self):
         print("yo", self.getNombre(), ",presidente, pongo el dinero para el entrenamiento")
 
-
+#Run
 e = Entrenador("Pedro")
 e2 = Futbolista("Juan")
 e3 = MedicoEquipo("Luis")
